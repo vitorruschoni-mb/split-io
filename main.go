c93176e57af8970446bc8ee2f50a9cd412a645fc
Vitor Ruschoni <vitorruschoni@gmail.com>
2022-07-04 11:13:24 -0300

    config changes

--- main.go
+++ main.go
@@ -11,13 +11,17 @@ import (
 
 func main() {
 	cfg := conf.Default()
-	cfg.LoggerConfig.LogLevel = logging.LevelError
+	cfg.TaskPeriods.SplitSync = 5
+	cfg.TaskPeriods.EventsSync = 5
+	cfg.TaskPeriods.TelemetrySync = 30
+	cfg.LoggerConfig.LogLevel = logging.LevelDebug
 
 	factory, err := client.NewSplitFactory("6buq76ob0v4up1h1itau80bahmena08h7olo", cfg)
 	if err != nil {
 		fmt.Printf("SDK init error: %s\n", err)
 		return
 	}
+
 	splitClient := factory.Client()
 	err = splitClient.BlockUntilReady(25)
 	if err != nil {
@@ -28,15 +32,18 @@ func main() {
 	for {
 		number := rand.Intn(5 - 1) + 1
 		getEnv(splitClient, number)
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 	}
 
 	splitClient.Destroy()
 }
 
 func getEnv(splitClient *client.SplitClient, userID int) {
-	treatment := splitClient.Treatment(userID, "FEATURE_FLAG_USER_ORDER_CREATE_SUS", map[string]interface{}{
+	treatment := splitClient.Treatment(fmt.Sprintf("%d", userID), "FEATURE_FLAG_USER_ORDER_CREATE_SUS", map[string]interface{}{
 		"USER_ID": fmt.Sprintf("%d", userID),
+		"BASE": "BTC",
+		"METHOD": "PLACE",
+		"order_type": "market",
 	})
 	fmt.Println("User ID:", userID, "Value:", treatment)
 }
